414c82f3492eed059df7bcadb2d78fe3b33aa73e
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-03-11 21:44:05 +0500

    Todo list edited ...

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const inputValue = document.querySelector('#todo').value;
-const addTodo = () =>{
-    const todo = document.createElement('li');
-    todo.innerText = inputValue;
-    document.querySelector('#todos').appendChild(todo);
+const addTodo = () => {
+ const inputValue = document.getElementById('todo').value;
+    if(!inputValue){
+        alert('Task should not be empty')
+        return;
+    }
+    // I want to save the input value in the local storage 
+
+    const todo = {
+        text: inputValue
+    };
     
+    const existingData = localStorage.getItem("todos");
+    let todos=existingData ? JSON.parse(existingData) : [];
+    todos.push();
+    localStorage.setItem("todos",JSON.stringify(todo));
+    console.log("saved successfully");
+    // const getData = localStorage.getItem("todos");
+    // const parsedData = JSON.parse(getData);
+    // parsedData append on the todos ID
+    const addLi=document.createElement('li')
+    addLi.innerHTML = todos.text;
+    document.getElementById('todos').appendChild(addLi);
+    document.getElementById("todo").value = "";
 
 }
